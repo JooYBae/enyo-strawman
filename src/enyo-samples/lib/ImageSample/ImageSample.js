c18789292b7f6a08918080863219c01c7f55b81a
rakesh25 <krakesh.cse@gmail.com>
2015-05-18 14:54:21 +0530

    Modified Image sample to include auto image placeholder check

--- src/enyo-samples/lib/ImageSample/ImageSample.js
+++ src/enyo-samples/lib/ImageSample/ImageSample.js
@@ -11,14 +11,14 @@ module.exports = kind({
 	kind: Control,
 	components: [
 		{content: "Image - No Sizing", classes: "section"},
-		{kind: "Image", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: Image, src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
 		{content: "Sizing - Contain", classes: "section"},
-		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(200,100,0,0.3); border: 1px dashed orange; margin-right: 1em;", sizing: "contain", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
-		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(0,200,0,0.3); border: 1px dashed green;", sizing: "contain", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: Image, style: "width:200px; height:100px; background-color:rgba(200,100,0,0.3); border: 1px dashed orange; margin-right: 1em;", sizing: "contain", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: Image, style: "width:200px; height:100px; background-color:rgba(0,200,0,0.3); border: 1px dashed green;", sizing: "contain", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
 		{content: "Sizing - Cover", classes: "section"},
-		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(0,0,200,0.3); border: 1px dashed blue; margin-right: 1em;", sizing: "cover", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
-		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(200,200,0,0.3); border: 1px dashed yellow;", sizing: "cover", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: Image, style: "width:200px; height:100px; background-color:rgba(0,0,200,0.3); border: 1px dashed blue; margin-right: 1em;", sizing: "cover", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: Image, style: "width:200px; height:100px; background-color:rgba(200,200,0,0.3); border: 1px dashed yellow;", sizing: "cover", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
 		{content: "Placeholder Image", classes: "section"},
-		{kind: "Image", src: Image.placeholder, alt: "Placeholder Image", classes: "placeholder"}
+		{kind: Image, src: Image.placeholder, alt: "Placeholder Image", classes: "placeholder"}
 	]
 });
