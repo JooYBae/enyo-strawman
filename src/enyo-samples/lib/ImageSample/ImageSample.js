f8b13ada20a4f899fa210459b323ea5dbbb87aa7
rakesh25 <krakesh.cse@gmail.com>
2015-05-18 14:46:19 +0530

    Modified Image sample to include auto image placeholder check

--- src/enyo-samples/lib/ImageSample/ImageSample.js
+++ src/enyo-samples/lib/ImageSample/ImageSample.js
@@ -10,11 +10,15 @@ module.exports = kind({
 	classes: "image-sample",
 	kind: Control,
 	components: [
-		{content: "Image", classes: "section"},
-		{kind: Image, src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
-		{content: "Background Image", classes: "section"},
-		{kind: Image, style: "width:200px;height:75px;background-color:gray", sizing: "constrain", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{content: "Image - No Sizing", classes: "section"},
+		{kind: "Image", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{content: "Sizing - Contain", classes: "section"},
+		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(200,100,0,0.3); border: 1px dashed orange; margin-right: 1em;", sizing: "contain", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(0,200,0,0.3); border: 1px dashed green;", sizing: "contain", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{content: "Sizing - Cover", classes: "section"},
+		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(0,0,200,0.3); border: 1px dashed blue; margin-right: 1em;", sizing: "cover", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{kind: "Image", style: "width:200px; height:100px; background-color:rgba(200,200,0,0.3); border: 1px dashed yellow;", sizing: "cover", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
 		{content: "Placeholder Image", classes: "section"},
-		{kind: Image, src: Image.placeholder, alt: "Placeholder Image", classes: "placeholder"}
+		{kind: "Image", src: Image.placeholder, alt: "Placeholder Image", classes: "placeholder"}
 	]
 });
